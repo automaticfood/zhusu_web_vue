26c1161dc1a53ea8f70fc18a93980d6f5b55bd55
automatic <23273891+automaticfood@users.noreply.github.com>
2023-12-26 20:33:32 +0800

    添加图片文件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
-
+  import tabbar from "@/components/tabbar/tabbar.vue"
 </script>
 
 <template>
   <RouterView></RouterView>
-  <router-link to="/home">主页</router-link>
-  <router-link to="/favor">收藏</router-link>
-  <router-link to="/message">消息</router-link>
-  <router-link to="/order">订单</router-link>
+  <tabbar></tabbar>
 </template>
 
 <style scoped>
---
+++ src/components/tabbar/tabbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="tabbar">
+        <div class="tabbar-item">
+            <img src="@/assets/img/tabbar/tab_home.png" alt="">
+            <span class="text">主页</span>    
+        </div>
+        <div class="tabbar-item">
+            <img src="@/assets/img/tabbar/tab_home.png" alt="">
+            <span class="text">收藏</span>  </div>
+        <div class="tabbar-item">消息</div>
+        <div class="tabbar-item">订单</div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+    .tabbar{
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 50px;
+        display: flex;
+        border-top: 1px solid #dcccbb;
+        .tabbar-item{
+            flex: 1;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    }
+</style>
