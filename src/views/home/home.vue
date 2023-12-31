62b14f2a7905be06d9f6dc494946f1e23f299b62
automatic <23273891+automaticfood@users.noreply.github.com>
2023-12-31 20:29:30 +0800

    开始添加home主页导航栏

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <div class="nav-bar">
+      <div class="title">深蓝之家</div>
+    </div>
     <h2>home</h2>
   </div>
 </template>
@@ -9,5 +12,16 @@
 </script>
 
 <style lang="less" scoped>
-  
+.nav-bar {
+  display: flex;
+  height: 36px;
+  justify-content: center;
+  align-items: center;
+}
+
+.title {
+  color: #ff6954;
+  font-size: 1.4em;
+  font-weight: 700;
+}
 </style>
