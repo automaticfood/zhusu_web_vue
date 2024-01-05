3b9f9dd018579207bf1d1afbc5bd2d0c7b3f44c0
automatic <23273891+automaticfood@users.noreply.github.com>
2024-01-05 20:28:13 +0800

    添加城市定位列表

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -2,13 +2,20 @@
   <div class="home">
     <HomeNavBar></HomeNavBar>
     <div class="banner">
-      <img src="@/assets/img/home/banner.webp" alt="">
+      <img src="@/assets/img/home/banner.webp" alt="" />
+    </div>
+    <div class="location">
+      <div class="city">北京</div>
+      <div class="position">
+        <span>我的位置</span>
+        <img src="@/assets/img/home/icon_location.png" alt="" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import HomeNavBar from "./cpns/home-nav-bar.vue"
+import HomeNavBar from "./cpns/home-nav-bar.vue";
 </script>
 
 <style lang="less" scoped>
@@ -16,6 +23,24 @@ import HomeNavBar from "./cpns/home-nav-bar.vue"
   .banner {
     img {
       width: 100%;
+    }
+  }
+}
+
+.location {
+  height: 36px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .city {
+    padding-left: 15px;
+  }
+  .position {
+    padding-right: 15px;
+    display: flex;
+    align-items: center;
+    img {
+      width: 26px;
     }
   }
 }
