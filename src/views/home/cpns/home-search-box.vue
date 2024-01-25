3a1c8837cd6e2e48fc44af6c91249db130eaa40b
automatic <23273891+automaticfood@users.noreply.github.com>
2024-01-25 23:55:49 +0800

    添加首页入住时间组件

--- src/views/home/cpns/home-search-box.vue
+++ src/views/home/cpns/home-search-box.vue
@@ -7,6 +7,22 @@
         <img src="@/assets/img/home/icon_location.png" alt="" />
       </div>
     </div>
+
+    <div class="item date-range">
+      <div class="start">
+        <div class="date">
+          <div class="tip">入住</div>
+          <div class="time">{{ startTime }}</div>
+        </div>
+        <div class="stay">共一晚</div>
+      </div>
+      <div class="end">
+        <div class="date">
+          <div class="tip">离店</div>
+          <div class="time">8月26日</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,6 +30,7 @@
 import router from "@/router";
 import useCityStore from "@/stores/modules/city";
 import { storeToRefs } from "pinia";
+import { ref } from "vue";
 
 const positionClick = () => {
   navigator.geolocation.getCurrentPosition(
@@ -32,15 +49,17 @@ const cityClick = () => {
 const cityStore = useCityStore()
 const { currentCity } = storeToRefs(cityStore)
 
+const startTime = ref(new Date("MM-DD"))
+
 </script>
 
 <style lang="less" scoped>
 .location {
-  height: 36px;
+  height: 44px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 5px 15px;
+  padding: 0 15px;
 
   .city {
     flex: 1;
@@ -52,6 +71,48 @@ const { currentCity } = storeToRefs(cityStore)
 
     img {
       width: 26px;
+    }
+  }
+}
+
+.item {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  padding: 0 20px;
+  color: #999;
+
+  .start {
+    flex: 1;
+    display: flex;
+    height: 44px;
+    align-items: center;
+    justify-content: space-between;
+
+    .stay {
+      margin-right: 40px;
+    }
+  }
+
+  .end {
+    min-width: 30%;
+    padding-left: 20px;
+  }
+
+  .date {
+    display: flex;
+    flex-direction: column;
+
+    .tip {
+      font-size: 12px;
+      color: #999;
+    }
+
+    .time {
+      margin-top: 3px;
+      color: #333;
+      font-size: 15px;
+      font-weight: 500;
     }
   }
 }
